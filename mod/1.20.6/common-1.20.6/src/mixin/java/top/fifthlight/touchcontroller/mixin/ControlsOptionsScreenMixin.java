7be1c2c14abc613fd09f7b9447d094d4bcce7f57
fifth_light <liu-yu-jie@foxmail.com>
2025-03-24 23:24:49 +0800

    修复 1.20.6 的配置界面入口

--- mod/1.20.6/common-1.20.6/src/mixin/java/top/fifthlight/touchcontroller/mixin/ControlsOptionsScreenMixin.java
+++ mod/1.20.6/common-1.20.6/src/mixin/java/top/fifthlight/touchcontroller/mixin/ControlsOptionsScreenMixin.java
@@ -2,10 +2,12 @@ package top.fifthlight.touchcontroller.mixin;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.OptionsList;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.controls.ControlsScreen;
 import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -13,18 +15,14 @@ import top.fifthlight.combine.platform_1_20_x.TextImpl;
 import top.fifthlight.touchcontroller.common.ui.screen.ConfigScreenKt;
 
 @Mixin(ControlsScreen.class)
-public abstract class ControlsOptionsScreenMixin extends Screen {
-    protected ControlsOptionsScreenMixin(Component pTitle) {
-        super(pTitle);
-    }
+public abstract class ControlsOptionsScreenMixin {
+    @Shadow
+    private OptionsList list;
 
     @Inject(at = @At("TAIL"), method = "init")
     protected void init(CallbackInfo ci) {
         var client = Minecraft.getInstance();
         var screen = (ControlsScreen) (Object) this;
-
-        var doneButton = (Button) screen.children().get(screen.children().size() - 1);
-        doneButton.setPosition(doneButton.getX(), doneButton.getY() + 24);
 
         var textObj = ConfigScreenKt.getConfigScreenButtonText();
         if (textObj instanceof TextImpl) {
@@ -32,14 +30,15 @@ public abstract class ControlsOptionsScreenMixin extends Screen {
         }
         Component text = (Component) textObj;
 
-        addRenderableWidget(
+        list.addSmall(
                 Button
                         .builder(
                                 text,
                                 btn -> client.setScreen((Screen) ConfigScreenKt.getConfigScreen(screen))
                         )
                         .bounds(screen.width / 2 - 155, screen.height / 6 + 60, 150, 20)
-                        .build()
+                        .build(),
+                null
         );
     }
 }
