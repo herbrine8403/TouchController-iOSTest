c9c566dc227e5813e2c4796971738e4739c8bb22
fifth_light <liu-yu-jie@foxmail.com>
2025-03-30 15:55:28 +0800

    修改老版本的文本生成，以支持换行符

--- mod/resources/src/main/kotlin/top/fifthlight/touchcontroller/resource/LegacyTextGenerator.kt
+++ mod/resources/src/main/kotlin/top/fifthlight/touchcontroller/resource/LegacyTextGenerator.kt
@@ -3,7 +3,9 @@ package top.fifthlight.touchcontroller.resource
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.decodeFromStream
+import java.io.StringWriter
 import java.nio.file.Path
+import java.util.Properties
 import kotlin.io.path.inputStream
 import kotlin.io.path.listDirectoryEntries
 import kotlin.io.path.nameWithoutExtension
@@ -15,10 +17,21 @@ fun generateLegacyText(languageDir: Path, legacyLanguageDir: Path) {
     for (file in languageFiles) {
         val outputFile = legacyLanguageDir.resolve("${file.nameWithoutExtension}.lang")
         val map: Map<String, String> = Json.decodeFromStream(file.inputStream())
-        outputFile.writer().use { writer ->
-            map.entries.sortedBy { (key, _) -> key }.forEach { (key, value) ->
-                writer.appendLine("$key=$value")
+        val writeBuffer = StringWriter()
+        Properties().apply {
+            map.entries.forEach { (key, value) ->
+                put(key, value)
             }
+        }.store(writeBuffer, "PARSE_ESCAPES")
+        outputFile.writer().use { writer ->
+            writeBuffer
+                .toString()
+                .lineSequence()
+                .filterIndexed { index, _ -> index != 1 }
+                .forEach {
+                    writer.write(it)
+                    writer.write('\n'.code)
+                }
         }
     }
 }
